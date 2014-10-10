d1e1e4bd3057a65b9fc79ef96f8605fad7e5c83c
= <lasazawa@gmail.com>
2014-10-10 15:09:54 -0700

    underscore tests passed

--- spec/test.js
+++ spec/test.js
@@ -4,53 +4,77 @@ var expect = require("chai").expect;
 // all of these tests are pending...make sure to add , function(){ expect ....}
 
 describe('first', function() {
-  it('should be able to pull out the first element of an array');
-    // expect(myFunctions.first([1,2,3])).to.equal(1);
-
-
-  it('should be able to accept a user-defined index first([1,2,3],2) should return [1,2]');
-
+  it('should be able to pull out the first element of an array', function(){
+    expect(myFunctions.first([1,2,3])).to.equal(1);
   });
+  it('should return entire array bc n is greater than array length',function(){
+    expect(myFunctions.first([1,2,3],5)).to.include.members([1,2,3]);
+  });
+  it('should be able to accept a user-defined index first([1,2,3,4,5],2) should return [1,2]',function(){
+    expect(myFunctions.first([1,2,3,4,5],2)).to.include.members([1,2]);
+  });
+});
 
 
 describe('last', function() {
-  it('should pull the last element from an array');
-
-  it('should accept an index argument');
-
-  it('should return nothing if zero is passed in as the index');
-
-  it('should return all the array\'s elements if the index argument is larger than the length of the array');
-
+  it('should pull the last element from an array', function() {
+    expect(myFunctions.last([1,2,3])).to.equal(3);
+  });
+  it('should accept an index argument', function() {
+    expect(myFunctions.last([1,2,3,4,5],2)).to.include.members([4,5]);
+  });
+  it('should return nothing if zero is passed in as the index', function() {
+    expect(myFunctions.last([1,2,3],0)).to.eql([]);
+  });
+  it('should return all the array\'s elements if the index argument is larger than the length of the array', function() {
+    expect(myFunctions.last([1,2,3],5)).to.include.members([1,2,3]);
+  });
 });
 
-describe('uniq', function() {
-  it('should return all unique values contained in an unsorted array');
 
+describe('uniq', function() {
+  it('should return all unique values contained in an unsorted array', function() {
+    expect(myFunctions.uniq([3,2,2,1,1,3])).to.include.members([1,2,3]);
+  });
 });
 
 
 describe('pluck', function() {
-  it('should return values contained at a user-defined property');
-    // var people = [
-    //   {name : 'moe', age : 30},
-    //   {name : 'curly', age : 50}
-    // ];
-
-    // expect(myFunctions.pluck(people, 'name')).to.eql(['moe', 'curly']);
+  it('should return values contained in property name', function() {
+    var people = [
+      {name : 'moe', age : 30},
+      {name : 'curly', age : 50}
+    ];
+    expect(myFunctions.pluck(people, 'name')).to.eql(['moe', 'curly']);
+  });
 });
 
 describe('contains', function() {
-  it('should return false if a collection does not contain a user-specified value');
+  it('should return false if a collection does not contain a user-specified value', function() {
+    expect(myFunctions.contains([1,2,3],4)).to.equal(false);
+  });
 
-  it('should return true if a collection contains a user-specified value');
-    // expect(myFunctions.contains([  4,   5,   6], 5)).to.equal(true);
-
+  it('should return true if a collection contains a user-specified value', function() {
+    expect(myFunctions.contains([1,2,3],3)).to.equal(true);
+  });
 });
 
 describe('flatten', function() {
-  it('can flatten nested arrays');
+  it('can flatten nested arrays', function() {
     // var nestedArray = [1, [2], [3, [[[4]]]]];
-
-    // expect(myFunctions.flatten(nestedArray)).to.eql([1,2,3,4]);
+    expect(myFunctions.flatten([1, [2], [3, [[[4]]]]])).to.eql([1,2,3,4]);
+  });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/underscore.js
+++ src/underscore.js
@@ -3,31 +3,69 @@ var myFunctions = {
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
   first: function(array, n) {
-
+    // check and see if n is undefined
+    // if n is greater than the length of the array, return the whole array
+    // (else) use splice to return a new array starting from the beginning and going till n
+    if (n === undefined) {
+      return array[0];
+    }
+    else if (n > array.length) {
+      return array;
+    }
+    else {
+      // return array.splice(array.length, array.length - n);
+      return array.splice(0,n);
+    }
   },
 
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   last: function(array, n) {
-
+    if (n === undefined) {
+      return array[array.length-1];
+    }
+    else if (n === 0) {
+      return [];
+    }
+    else if (n > array.length) {
+      return array;
+    }
+    else {
+      return array.splice(n, array.length - 1);
+    }
   },
 
   // Produce a duplicate-free version of the array.
   uniq: function(array) {
-
+    array.sort();
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] === array[i+1]) {
+        array.splice(array[i],1);
+      }
+    }
+    return array;
   },
 
   // Takes an array of objects and returns and array of the values of
   // a certain property in it. E.g. take an array of people objects
   // (which have a name and an age) and return an array of just their ages
   pluck: function(array, key) {
-
+    var newArr = [];
+    for (var i = 0; i < array.length; i++) {
+      newArr.push(array[i].name);
+    }
+    return newArr;
   },
 
  // Determine if the array contains a given value (using `===`).
  // TIP: There are multiple solutions to this problem, ranging from one line to using reduce();
   contains: function(array, target) {
-
+    if (array.indexOf(target) != -1) {
+      return true;
+    }
+    else {
+      return false;
+    }
   },
 
   // Turns a multidimensional array into a single array
@@ -36,7 +74,18 @@ var myFunctions = {
   // Also, you will need to use recursion
   // when you find an array inside of an array
   flatten: function(array) {
-
+    var newArr = [];
+    array.forEach(function(value) {
+      if (value.constructor.name === "Array") {
+        newArr = newArr.concat(myFunctions.flatten(value));
+      }
+      else {
+        newArr.push(value);
+      }
+    });
+    return newArr;
   }
 };
+
+
 module.exports = myFunctions;
